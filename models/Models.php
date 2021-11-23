589d33603d6103c0b5c934051a154fc851f063a4
vbadkar <vivekbadkar@gmail.com>
2021-11-24 00:32:29 +0530

    #3: Established database connection and added query for fetching user details in model file

---
+++ models/Models.php
@@ -0,0 +1,17 @@
+<?php
+require "dbconfig.php";
+class Database{
+    function db_connect(){
+        $con = mysqli_connect($database['host'], $database['user'], $database['password'], $database['dbName']);
+        if (!$con) {
+            die("Connection to database failed");
+        }
+    }
+}
+class Contact{
+    function fetchUserDetails($id){
+        $sql = "SELECT * FROM users WHERE id = '$id' ";
+        $result = mysqli_query($con, $sql);
+        return $row = mysqli_fetch_assoc($result);
+    }
+}
