e924ecc381370646e40013f305bfa15043c3f1b4
RChaubey16 <ruturajchaubey16@gmail.com>
2021-11-24 15:43:22 +0530

    #6: Added the twig template engine

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php 
-    echo "Hey!!!";
-   
+<?php
+
+// require_once __DIR__.'/bootstrap.php';
+require_once('./twigtemplate.php');
+
+// Sample data
+$foo = [
+  [ 'name'          => 'Alice' ],
+  [ 'name'          => 'Bob' ],
+  [ 'name'          => 'Eve' ],
+];
+
+// Render our view
+echo $twig->render('home.html.twig', ['foo' => $foo] );
--- routes/route.php
+++ routes/route.php
@@ -1,14 +1,17 @@
 <?php 
 
+    require_once('../twigtemplate.php');
+
     // echo "Hey You are here";
     $path = $_SERVER['REQUEST_URI'];
     echo $path;
 
     switch ($path){
         case "/custom_cms/home":
-            header("Location: index.php");
-            Home::CreateView();
+            // header("Location: index.php");
+            // Home::CreateView();
             // echo "Home !";
+            echo $twig->render('home.html.twig' , ['name' => "Raj"]);
             break;
         // default:
         //     echo "Error 404";
