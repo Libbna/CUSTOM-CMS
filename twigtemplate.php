39b9ff27f3c2ded7f5ab8d79e744459627a1a676
RChaubey16 <ruturajchaubey16@gmail.com>
2021-11-24 15:46:03 +0530

    #6: Created the directory structure for views and code cleanup

--- twigtemplate.php
+++ twigtemplate.php
@@ -1,14 +1,9 @@
-
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-// require("./vendor/autoload");
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-$loader = new FilesystemLoader(__DIR__ . '/templates');
+$loader = new FilesystemLoader(__DIR__ . '/views/templates/');
 $twig = new Environment($loader);
-
-// echo $twig->render('first.html.twig', ['name' => 'John Doe', 
-//     'occupation' => 'gardener']);
