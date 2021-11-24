bb748b84d76a9142335ab63cd3cd24e9fc0ef283
Libbna <libbna260296@gmail.com>
2021-11-24 21:44:19 +0530

    #7 created a new controller Contact

--- controllers/Home.php
+++ controllers/Home.php
@@ -1,12 +1,18 @@
 <?php
 require_once('Controller.php');
+
 class Home extends Controller
 {
+    public function __construct()
+    {
+        require_once __DIR__ . "../models/Models.php";
+        
+    }
     public function getData($twig)
     {
         $data = "Dummy Text";
         echo $twig->render('home.html.twig', ['name' => $data]);
         return;
-    }
+    }   
 }
 
