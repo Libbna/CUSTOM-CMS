9b631003c8dd8a5d641d2885caa6a273cb940116
RChaubey16 <ruturajchaubey16@gmail.com>
2021-12-09 18:24:44 +0530

    #24: Shifted the symfony-routing code to route.php filr

--- routes/route.php
+++ routes/route.php
@@ -1,27 +1,58 @@
-<?php 
-    require_once('../twigtemplate.php');
-    require_once('../controllers/Home.php');
-    require_once('../controllers/Contact.php');
+<?php
 
-    
-    $url = $_SERVER['REQUEST_URI'];  
-    $org_path = explode('/', $url);
-    $path = $org_path[2];  
-    switch ($path){
-        case "home":
-            $object = new Home();
-            $response = $object->getData($twig);
-            break;
-        case "home?insert":
-            $object = new Contacts();
-            $object->insertUser($twig);
-            break;
-        case "contact":
-            $object = new Contacts();
-            $response = $object->fetchUser($twig);
-            break;
-        default:
-            echo "Error 404";
-    }
 
-    
+require_once '../vendor/autoload.php';
+require_once '../controllers/Home.php';
+require_once '../controllers/Contact.php';
+require_once '../twigtemplate.php';
+
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+
+try {
+    $routes = new RouteCollection();
+    $context = new RequestContext();
+
+    // Create Routes
+    $home_route = new Route("/custom_cms/home", ['controller' => "Home::getData"]);
+    $contact_route = new Route("/custom_cms/contact", array('controller' => "Contacts::fetchUser"));
+
+    // Parameterized route example
+    $foo_placeholder_route = new Route(
+        '/custom_cms/foo/{id}',
+        array('controller' => 'Home::getData'),
+        array('id' => '[0-9]+')
+    );
+
+    // Add routes to collection
+    $routes->add('home_route', $home_route);
+    $routes->add('contact_route', $contact_route);
+    $routes->add('foo_route', $foo_placeholder_route);
+
+    // Match the incoming route
+    $context->fromRequest(Request::createFromGlobals());
+    $matcher = new UrlMatcher($routes, $context);
+    $parameters = $matcher->match($context->getPathInfo());
+
+    // Dump the params of the route matched
+    // echo '<pre>';
+    // print_r($parameters);
+    // var_dump($parameters);
+
+    // Execution
+    list($controllerClassName, $action) = explode('::', $parameters['controller']);
+    $controller = new $controllerClassName();
+    $controller->{$action}($twig);
+
+
+    exit;
+} catch (ResourceNotFoundException $e) {
+    echo $e->getMessage();
+    var_dump($e);
+}
