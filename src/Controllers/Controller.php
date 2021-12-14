4342368e52fe7c000057222bbefedcdc11994e85
Libbna <46792570+Libbna@users.noreply.github.com>
2021-12-14 13:36:28 +0530

    Removed comments

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -1,20 +1,11 @@
 <?php
 
-// namespace App\Controllers;
 namespace CustomCms\Controllers;
 
 
 
 class Controller {
-    // protected $db;
-
-    // public function __construct()
-    // {
-    //     require_once __DIR__ . "../models/Models.php";
-
-    //     $this->$db = new Database;
-    // }
-    
+        
     public static function CreateView() {
         echo "View Created";
     }   
