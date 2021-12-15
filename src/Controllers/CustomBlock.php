e1f5853e2086073cd31294924ce560203903b3cf
RChaubey16 <ruturajchaubey16@gmail.com>
2021-12-15 16:57:49 +0530

    #43: Updated the controller function

--- src/Controllers/CustomBlock.php
+++ src/Controllers/CustomBlock.php
@@ -9,6 +9,15 @@ class CustomBlock
     }
 
     public function customBlockInfo($twig){
+
+        $block_title = $_POST['block-title'];
+        $block_body = $_POST['block-description'];
+
+        if (!isset($block_title) and !isset($block_body)){
+            echo $twig->render('error.html.twig');
+            return;
+        }
+
         echo $twig->render('blockDisplay.html.twig');
         return;
     }
