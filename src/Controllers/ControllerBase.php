60af720b817da158340ba0caa90429dbd11ba9c9
vbadkar <vivekbadkar@gmail.com>
2022-01-03 22:58:20 +0530

    #130: Resolved the issue with hardcoded base path and made it dynamic

--- src/Controllers/ControllerBase.php
+++ src/Controllers/ControllerBase.php
@@ -10,6 +10,7 @@ class ControllerBase{
     protected function preprocessPage(){
 
         $this->page['nav_links'] = $this->getNavLinks();
+        $this->page['base_url'] = $this->getBaseUrl();
         return $this->page;
     }
     protected function getNavLinks(){
@@ -18,5 +19,10 @@ class ControllerBase{
         $result = $displayMenu->displayMenu();
         return $result;
 
+    }
+    protected function getBaseUrl(){
+        $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+        $base = explode("route", $url);
+        return $base;
     }
 } 
