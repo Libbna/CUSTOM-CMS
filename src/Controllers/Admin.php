80f2997d727feb74b80658f7befa7edb789341e8
Libbna <libbna260296@gmail.com>
2022-01-10 18:23:31 +0530

    #147 dynamic logo and site name completed

--- src/Controllers/Admin.php
+++ src/Controllers/Admin.php
@@ -7,20 +7,19 @@ use Cms\Models\AdminModel;
 
 class Admin extends ControllerBase
 {
-
-    public function displayUserDetails($twig){
-
+    public function displayUserDetails($twig)
+    {
         $variables = parent::preprocesspage();
         if ($_SESSION['role'] != 'admin') {
             $variables['authenticated_userId'] = $_SESSION['user_id'];
-            $variables['message'] = "Access Prohibited!";
-            echo $twig->render("error.html.twig", $variables);
+            $variables['message'] = 'Access Prohibited!';
+            echo $twig->render('error.html.twig', $variables);
             return;
         }
         $displayUsers = new AdminModel();
         $result = $displayUsers->displayUsers();
         $variables['result'] = $result;
-        if (isset($_SESSION["user_id"])){
+        if (isset($_SESSION['user_id'])) {
             $variables['username'] = $_SESSION['username'];
             $variables['authenticated_userId'] = $_SESSION['user_id'];
             $variables['role'] = $_SESSION['role'];
@@ -29,54 +28,57 @@ class Admin extends ControllerBase
         return;
     }
 
-    public function updateUserRoleToAdmin($twig, $id){
+    public function updateUserRoleToAdmin($twig, $id)
+    {
         $variables = parent::preprocesspage();
 
         if ($_SESSION['role'] != 'admin') {
             $variables['authenticated_userId'] = $_SESSION['user_id'];
-            $variables['message'] = "Access Prohibited!";
-            echo $twig->render("error.html.twig", $variables);
+            $variables['message'] = 'Access Prohibited!';
+            echo $twig->render('error.html.twig', $variables);
             return;
         }
 
         $userRole = new AdminModel();
         $result = $userRole->setUserToAdmin($id);
-        if (empty($result) == 1){
-            $variables['status'] = "true";
-            $variables['message'] = "User is now an Administrator!";
-            $variables['title'] = $this->reverie . " | Users";
-            echo $twig->render("userDisplay.html.twig", $variables);
+        if (empty($result) == 1) {
+            $variables['status'] = 'true';
+            $variables['message'] = 'User is now an Administrator!';
+            $variables['title'] = $this->reverie . ' | Users';
+            echo $twig->render('userDisplay.html.twig', $variables);
             return;
         }
         return;
     }
 
-    public function updateUserRoleToAuth($twig, $id){
+    public function updateUserRoleToAuth($twig, $id)
+    {
         $variables = parent::preprocesspage();
 
         if ($_SESSION['role'] != 'admin') {
             $variables['authenticated_userId'] = $_SESSION['user_id'];
-            $variables['message'] = "Access Prohibited!";
-            echo $twig->render("error.html.twig", $variables);
+            $variables['message'] = 'Access Prohibited!';
+            echo $twig->render('error.html.twig', $variables);
             return;
         }
 
         $userRole = new AdminModel();
         $result = $userRole->setUserToAuth($id);
-        if (empty($result) == 1){
-            $variables['status'] = "true";
-            $variables['message'] = "User is now an Administrator!";
-            $variables['title'] = $this->reverie . " | Users";
-            echo $twig->render("userDisplay.html.twig", $variables);
+        if (empty($result) == 1) {
+            $variables['status'] = 'true';
+            $variables['message'] = 'User is now an Administrator!';
+            $variables['title'] = $this->reverie . ' | Users';
+            echo $twig->render('userDisplay.html.twig', $variables);
             return;
         }
         return;
     }
-    public function userDelete($twig, $id){
+    public function userDelete($twig, $id)
+    {
         $variables = parent::preprocesspage();
         $deleteUser = new AdminModel();
         $deleteUser->deleteUser($id);
-        if (isset($_SESSION["user_id"])){
+        if (isset($_SESSION['user_id'])) {
             $variables['username'] = $_SESSION['username'];
             $variables['authenticated_userId'] = $_SESSION['user_id'];
             $variables['role'] = $_SESSION['role'];
@@ -84,30 +86,28 @@ class Admin extends ControllerBase
         $displayUsers = new AdminModel();
         $result = $displayUsers->displayUsers();
         $variables['result'] = $result;
-        $variables['message'] = "User deleted successfully";
+        $variables['message'] = 'User deleted successfully';
         echo $twig->render('userDisplay.html.twig', $variables);
         return;
     }
 
-    public function getConfigForm($twig) {
+    public function getConfigForm($twig)
+    {
         $variables = parent::preprocessPage();
         echo $twig->render('config.html.twig', $variables);
         return;
     }
-    
-    public function configDetails($twig) {
+
+    public function configDetails($twig)
+    {
         $variables = parent::preprocessPage();
 
-        $logo_upld = $_FILES['logo_upload'];
         $site_name = $_POST['site_name'];
 
-        // dump($_POST);
-        // exit;
-
-        $insertLogo = new AdminModel();
-        $ans = $insertLogo->insertLogo($site_name);
+        $updateLogo = new AdminModel();
+        $ans = $updateLogo->updateLogo($site_name);
         if ($_SESSION['role'] == 'admin') {
-            if (isset($_SESSION["user_id"])) {
+            if (isset($_SESSION['user_id'])) {
                 if (empty($ans) == 1) {
                     $variables['username'] = $_SESSION['username'];
                     $variables['authenticated_userId'] = $_SESSION['user_id'];
@@ -117,11 +117,19 @@ class Admin extends ControllerBase
                 }
                 return;
             }
-            
         }
-        
     }
 
+    public function displayLogo($twig)
+    {
+        $variables = parent::preprocessPage();
 
-    
+        $displayLogo = new AdminModel();
+        $result = $displayLogo->displayLogo();
+        $variables['result'] = $result;
+
+        echo $twig->render('header.html.twig', $variables);
+
+        return;
+    }
 }
--- src/Models/AdminModel.php
+++ src/Models/AdminModel.php
@@ -5,7 +5,6 @@ namespace Cms\Models;
 require 'vendor/autoload.php';
 
 use Intervention\Image\ImageManager;
-use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Intervention\Image\ImageManagerStatic as Image;
 
 class AdminModel
@@ -13,46 +12,58 @@ class AdminModel
     public $conn;
     public $result;
     public $sql;
-    
+
     //establishing database connection
     public function __construct()
     {
         require './dbconfig.php';
 
-        $this->conn = mysqli_connect($database['host'], $database['user'], $database['password'], $database['dbName']);
+        $this->conn = mysqli_connect(
+            $database['host'],
+            $database['user'],
+            $database['password'],
+            $database['dbName']
+        );
         if (!$this->conn) {
-            echo "<h1>Datbase connection failed</h1>";
+            echo '<h1>Datbase connection failed</h1>';
             return;
         }
     }
 
-    // query for displaying Users 
-    public function displayUsers() {
-        $query = $this->conn->prepare("SELECT * FROM userauth");
+    // query for displaying Users
+    public function displayUsers()
+    {
+        $query = $this->conn->prepare('SELECT * FROM userauth');
         $query->execute();
         $ans = $query->get_result();
         return $ans;
     }
 
-    public function setUserToAdmin($user_id){
+    public function setUserToAdmin($user_id)
+    {
         // UPDATE userdetails SET bio = ? WHERE user_id = ?
-        $query = $this->conn->prepare("UPDATE userauth SET roles = ? WHERE id = ?");
-        $query->bind_param("si", $role, $user_id);
-        $role = "admin";
+        $query = $this->conn->prepare(
+            'UPDATE userauth SET roles = ? WHERE id = ?'
+        );
+        $query->bind_param('si', $role, $user_id);
+        $role = 'admin';
         $query->execute();
         $ans = $query->get_result();
         return $ans;
     }
 
-    public function setUserToAuth($user_id){
-        $authenticated = "authenticated"; 
-        $query = $this->conn->prepare("UPDATE userauth SET roles = ? WHERE id = ?");
-        $query->bind_param("si", $role, $user_id);
+    public function setUserToAuth($user_id)
+    {
+        $authenticated = 'authenticated';
+        $query = $this->conn->prepare(
+            'UPDATE userauth SET roles = ? WHERE id = ?'
+        );
+        $query->bind_param('si', $role, $user_id);
         $role = $authenticated;
 
         // if logged in user who is also admin, requests to change the role.
-        if ($_SESSION['user_id'] == $user_id){
-            if ($_SESSION['role'] == 'admin'){
+        if ($_SESSION['user_id'] == $user_id) {
+            if ($_SESSION['role'] == 'admin') {
                 $_SESSION['role'] = $authenticated;
             }
         }
@@ -61,25 +72,25 @@ class AdminModel
         $ans = $query->get_result();
         return $ans;
     }
-    public function deleteUser($id){
-        $query = $this->conn->prepare("DELETE FROM userauth WHERE id = ?");
-        $query->bind_param("i", $id);
+    public function deleteUser($id)
+    {
+        $query = $this->conn->prepare('DELETE FROM userauth WHERE id = ?');
+        $query->bind_param('i', $id);
         $query->execute();
         return;
     }
 
     // funtion insert logo
-    public function insertLogo($site_name) {
+    public function updateLogo($site_name)
+    {
         $logo = $_FILES['logo_upload'];
         $file = $logo['name'];
         $file_tmp = $logo['tmp_name'];
-        
-        $profile_ext = explode('.',  $file);
+
+        $profile_ext = explode('.', $file);
         $filecheck = strtolower(end($profile_ext));
 
-        $file_ext_stored = array('jpeg', 'png', 'jpg');
-
-        
+        $file_ext_stored = ['jpeg', 'png', 'jpg'];
 
         // location
         $location = 'assets/images/logo/' . $file;
@@ -90,23 +101,31 @@ class AdminModel
         $file_extension = strtolower($file_extension);
 
         if (in_array($filecheck, $file_ext_stored)) {
-
             $manager = new ImageManager(['driver' => 'gd']);
 
-
-            $img = Image::make($file_tmp)->resize(50, 50)->save($resized_loc);
-
+            $img = Image::make($file_tmp)
+                ->resize(50, 50)
+                ->save($resized_loc);
 
             move_uploaded_file($img, $resized_loc);
-
         }
-        $query = $this->conn->prepare("INSERT INTO config(logo, siteName) VALUES(?, ?)");
-        $query->bind_param("ss",   $location, $site_name);
+        $query = $this->conn->prepare(
+            'UPDATE config SET logo = ?, siteName = ? WHERE id = 22'
+        );
+        $query->bind_param('ss', $resized_loc, $site_name);
         $query->execute();
         $ans = $query->get_result();
         return $ans;
     }
 
-    
-   
+    // display logo
+    public function displayLogo()
+    {
+        $query = $this->conn->prepare(
+            'SELECT * FROM config ORDER BY id DESC LIMIT 1'
+        );
+        $query->execute();
+        $ans = $query->get_result();
+        return $ans;
+    }
 }
