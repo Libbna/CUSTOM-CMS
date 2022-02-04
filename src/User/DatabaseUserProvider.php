c7f0bc2fd7e177c5ef869f9ba128546c226178ee
Libbna <libbna260296@gmail.com>
2022-02-04 18:28:09 +0530

    #263 resolved the coding standards issues of User/ files

--- src/User/DatabaseUserProvider.php
+++ src/User/DatabaseUserProvider.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Database methods to store and fetch user details.
+ *
+ * @category Content_Management_System
+ * @package  User_Database
+ * @author   Author <author@gmail.com>
+ * @license  License v3
+ * @link     https://github.com/Libbna/CUSTOM-CMS#readme
+ */
 namespace Cms\User;
 
 session_start();
@@ -9,61 +18,120 @@ use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Cms\User\User;
 
+/**
+ * A class DatabaseUserProvider implementing UserProviderInterface.
+ *
+ * @category Content_Management_System
+ * @package  User
+ * @author   Author <author@gmail.com>
+ * @license  License v3
+ * @link     https://github.com/Libbna/CUSTOM-CMS#readme
+ */
 class DatabaseUserProvider implements UserProviderInterface
 {
     public $connection;
-
+    /**
+     * Constructs connection variable.
+     *
+     * @param $connection Establish Connection.
+     */
     public function __construct($connection)
     {
         $this->connection = $connection;
     }
-
+    /**
+     * Loads the user for the given username.
+     *
+     * @param $username Name of he user.
+     *
+     * @return void
+     */
     public function loadUserByUsername($username)
     {
         return $this->getUser($username);
     }
-
+    /**
+     * To fetch the user.
+     *
+     * @param $username Name of he user.
+     *
+     * @return void
+     */
     public function getUser($username)
     {
-        $stmt = $this->connection->prepare("SELECT * FROM userauth WHERE username = ?");
-        $stmt->bind_param("s", $username);
+        $stmt = $this->connection->prepare(
+            'SELECT * FROM userauth WHERE username = ?'
+        );
+        $stmt->bind_param('s', $username);
         $stmt->execute();
         $ans = $stmt->get_result();
         $row = $ans->fetch_assoc();
         $id = $row['id'];
 
-
-
-        if (!$row['username'])
-        {
-            $exception = new UsernameNotFoundException(sprintf('Username "%s" not found in the database.', $row['username']));
+        if (!$row['username']) {
+            $exception = new UsernameNotFoundException(
+                sprintf(
+                    'Username "%s" not found in the database.',
+                    $row['username']
+                )
+            );
             $exception->setUsername($username);
             throw $exception;
-        }
-        else
-        {
-            return new User($row['username'], $row['password'], $row['roles'], $id);
+        } else {
+            return new User(
+                $row['username'],
+                $row['password'],
+                $row['roles'],
+                $id
+            );
         }
     }
-
-    public function insertUser($username, $password, $role){
-        $stmt = $this->connection->prepare("INSERT INTO userauth(username, password, roles) VALUES(?, ?, ?)");
-        $stmt->bind_param("sss", $username, $password, $role);
+    /**
+     * To insert user details.
+     *
+     * @param $username Name of the user.
+     * @param $password Password of the user.
+     * @param $role     User Role.
+     *
+     * @return void
+     */
+    public function insertUser($username, $password, $role)
+    {
+        $stmt = $this->connection->prepare(
+            'INSERT INTO userauth(username, password, roles) VALUES(?, ?, ?)'
+        );
+        $stmt->bind_param('sss', $username, $password, $role);
         $stmt->execute();
         $ans = $stmt->get_result();
         return true;
     }
-
+    /**
+     * Refreshes the user.
+     *
+     * @param $user UserInterface variable.
+     *
+     * @return void
+     */
     public function refreshUser(UserInterface $user)
     {
-        if (!$user instanceof User)
-        {
-            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(
+                sprintf(
+                    'Instances of "%s" are not supported.',
+                    get_class($user)
+                )
+            );
         }
 
         return $this->getUser($user->getUsername());
     }
-
+    /**
+     * Whether this provider supports the given user class.
+     *
+     * @param $class Class variable.
+     *
+     * @return void
+     */
     public function supportsClass($class)
     {
         return 'Cms\User\User' === $class;
--- src/User/User.php
+++ src/User/User.php
@@ -1,52 +1,104 @@
 <?php
+/**
+ * Set the user details like name, password, email, etc.
+ *
+ * @category Content_Management_System
+ * @package  User
+ * @author   Author <author@gmail.com>
+ * @license  License v3
+ * @link     https://github.com/Libbna/CUSTOM-CMS#readme
+ */
 namespace Cms\User;
 
 use Symfony\Component\Security\Core\User\UserInterface;
-
+/**
+ * A class representing a user interfaces methods.
+ *
+ * @category Content_Management_System
+ * @package  User
+ * @author   Author <author@gmail.com>
+ * @license  License v3
+ * @link     https://github.com/Libbna/CUSTOM-CMS#readme
+ */
 class User implements UserInterface
 {
-    private $username;
-    private $password;
-    private $roles;
-
-    public function __construct(string $username, string $password, string $roles, int $user_id)
-    {
-        if (empty($username))
-        {
+    public $username;
+    public $password;
+    public $roles;
+    /**
+     * Contructs username, password, role and user id objects.
+     *
+     * @param string $username Name of the user.
+     * @param string $password Password of the user.
+     * @param string $roles    User Role.
+     * @param string $user_id  User id.
+     */
+    public function __construct(
+        string $username,
+        string $password,
+        string $roles,
+        int $user_id
+    ) {
+        if (empty($username)) {
             throw new \InvalidArgumentException('No username provided.');
         }
-
         $this->username = $username;
         $this->password = $password;
         $this->roles = $roles;
         $this->user_id = $user_id;
     }
-
+    /**
+     * Method to fetch the username.
+     *
+     * @return void
+     */
     public function getUsername()
     {
         return $this->username;
     }
-
+    /**
+     * Method to fetch the password.
+     *
+     * @return void
+     */
     public function getPassword()
     {
         return $this->password;
     }
-
+    /**
+     * Method to fetch the roles to a user.
+     *
+     * @return void
+     */
     public function getRoles()
     {
-        $role = explode(",", $this->roles);
+        $role = explode(',', $this->roles);
         return $role[0];
     }
-
+    /**
+     * Method to fetch the user id.
+     *
+     * @return void
+     */
     public function getUserId()
     {
         return $this->user_id;
     }
-
+    /**
+     * Returns the salt that was originaly used to encode a password.
+     *
+     * @return void
+     */
     public function getSalt()
     {
         return '';
     }
-
-    public function eraseCredentials() {}
+    /**
+     * Removes the sensitve data from the user.
+     *
+     * @return void
+     */
+    public function eraseCredentials()
+    {
+    }
 }
